e9aac03a23eee9627992b18b54cf4d7e19a72bd7
FezChat <frediezra001@gmail.com>
2025-11-26 17:57:36 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -11,7 +11,7 @@ const {
     delay,
     makeCacheableSignalKeyStore,
     Browsers
-} = require('@whiskeysokets/baileys');
+} = require('@whiskeysockets/baileys');
 
 function removeFile(FilePath) {
     if (!fs.existsSync(FilePath)) return false;
