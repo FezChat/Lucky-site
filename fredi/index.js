da3178dd501e9a7cb1f8d7cda7378ee98eb90906
FezChat <frediezra001@gmail.com>
2025-11-28 19:48:22 +0300

    Update index.js

--- fredi/index.js
+++ fredi/index.js
@@ -1,41 +1,10 @@
-const fs = require('fs');
-
-// Generates a random ID of given length (default: 4)
-function frediId(length = 4) {
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+function makeid(num = 4) {
   let result = "";
-
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var characters9 = characters.length;
+  for (var i = 0; i < num; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters9));
   }
-  
   return result;
 }
-
-// Generates a random 8-character code
-function generateRandomCode() {
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  let result = '';
-  
-  for (let i = 0; i < 8; i++) {
-    result += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-  
-  return result;
-}
-
-// Removes a file or directory, returns a promise
-async function removeFile(filePath) {
-  try {
-    if (fs.existsSync(filePath)) {
-      await fs.promises.rm(filePath, { recursive: true, force: true });
-      return true;
-    }
-    return false;
-  } catch (error) {
-    console.error(`Error removing file: ${filePath}`, error);
-    return false;
-  }
-}
-
-module.exports = { frediId, removeFile, generateRandomCode };
+module.exports = {makeid};
